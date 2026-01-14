efca8d0f6b9054b43c5e3e72e7b33a1a81958e55
Pete Lippincott <petesimple@gmail.com>
2026-01-14 16:44:32 -0600

    Add common words for puck-related terminology

---
+++ words-common.js
@@ -0,0 +1,7 @@
+window.PUCKLE_COMMON_WORDS = [
+  "pucks","goalie","rails","quick","banks","angle","wrist","slide","stick",
+  "speed","skill","shots","slash","bump","score","timer","round","table",
+  "light","front","serve","touch","grind","clean","tight","shake","focus",
+  "edges","curve","glide","blade","press","smash","snipe","track","pivot",
+  "sweep","flush","brisk","flick","crazy","zones","house","feint","drill"
+].map(w => w.toLowerCase());
